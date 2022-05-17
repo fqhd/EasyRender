8576f4f0fd1ec1a3313ec8b1a383d7f447f8fd1e
fqhd <fahd31256@gmail.com>
2022-05-17 14:18:01 +0100

    fix: init gl state

--- easy_render.js
+++ easy_render.js
@@ -1,12 +1,24 @@
 let gl;
 
 function ERInit() {
+	if (initWebGL() == -1) {
+		return;
+	}
+	initGLState();
+}
+
+function initWebGL() {
 	const canvas = document.getElementById("ERCanvas");
 	gl = canvas.getContext("WebGL");
 	if (gl === null) {
 		alert(
 			"Unable to initialize WebGL. Your browser or machine may not support it."
 		);
-		return;
+		return -1;
 	}
+	return 1;
+}
+
+function initGLState() {
+	gl.clearColor(0, 0, 0, 0);
 }
