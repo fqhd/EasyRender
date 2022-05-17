d2739bdf9b450f1a0f151acce8652b4bf5f293c1
fqhd <fahd31256@gmail.com>
2022-05-17 14:16:16 +0100

    fix: webgl initialization

--- easy_render.js
+++ easy_render.js
@@ -1,5 +1,12 @@
 let gl;
 
-function ERInit(){
-
+function ERInit() {
+	const canvas = document.getElementById("ERCanvas");
+	gl = canvas.getContext("WebGL");
+	if (gl === null) {
+		alert(
+			"Unable to initialize WebGL. Your browser or machine may not support it."
+		);
+		return;
+	}
 }
