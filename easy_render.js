24a3eecf62f91452fdd5e9fed1848a332124385a
fqhd <fahd31256@gmail.com>
2022-05-24 16:20:25 +0100

    fix: texture loading functions

--- easy_render.js
+++ easy_render.js
@@ -187,31 +187,34 @@ function ERCreateTexture(data, w, h) {
 	return texture;
 }
 
-// TODO: repair this func
-function ERLoadTexture(url) {
-	const image = new Image();
-	image.onload = function () {
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.texImage2D(
-			gl.TEXTURE_2D,
-			level,
-			internalFormat,
-			srcFormat,
-			srcType,
-			image
-		);
-
-		if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-			gl.generateMipmap(gl.TEXTURE_2D);
-		} else {
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-		}
-	};
-	image.src = url;
-
-	return texture;
+async function ERLoadTexture(url) {
+	return new Promise((resolve, reject) => {
+		const image = new Image();
+		image.src = url;
+		image.onload = function () {
+			const texture = gl.createTexture();
+			gl.bindTexture(gl.TEXTURE_2D, texture);
+			gl.texImage2D(
+				gl.TEXTURE_2D,
+				0,
+				gl.RGBA,
+				gl.RGBA,
+				gl.UNSIGNED_BYTE,
+				image
+			);
+			if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+				gl.generateMipmap(gl.TEXTURE_2D);
+			} else {
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+			}
+			resolve(texture);
+		};
+		image.onabort = function () {
+			reject("ERLoadTexture: failed to load texture: " + url);
+		};
+	});
 }
 
 function isPowerOf2(value) {
--- main.js
+++ main.js
@@ -1,30 +1,33 @@
-// Initialize the library
-ERInit();
+async function main() {
+	// Initialize the library
+	ERInit();
 
-// Define the positions, normals, and textureCoords of your model (These will generally be loaded from a file)
-const positions = [0, 0, 0, 0.5, 1, 0, 1, 0, 0];
-const normals = [0, 0, -1, 0, 0, -1, 0, 0, -1];
-const textureCoords = [0, 0, 0.5, 1, 1, 0];
-const indices = [0, 1, 2];
+	// Define the positions, normals, and textureCoords of your model (These will generally be loaded from a file)
+	const positions = [0, 0, 0, 0.5, 1, 0, 1, 0, 0];
+	const normals = [0, 0, -1, 0, 0, -1, 0, 0, -1];
+	const textureCoords = [0, 0, 0.5, 1, 1, 0];
+	const indices = [0, 1, 2];
 
-// Define the textureData of your model(This will also generally be loaded in from a file)
-const textureData = [
-	0,
-	0,
-	255,
-	255, // This will make 1 opaque blue pixel
-];
+	// Define the textureData of your model(This will also generally be loaded in from a file)
+	const textureData = [
+		0,
+		0,
+		255,
+		255, // This will make 1 opaque blue pixel
+	];
 
-const model = ERCreateModel(positions, normals, indices, textureCoords);
-const texture = ERCreateTexture(textureData, 1, 1);
-const ourObject = ERCreateObject(model, texture);
+	const model = ERCreateModel(positions, normals, indices, textureCoords);
+	const texture = await ERLoadTexture("./bricks.png");
+	const ourObject = ERCreateObject(model, texture);
 
-// Make a list of the objects you would like to draw
-const objects = [ourObject];
+	// Make a list of the objects you would like to draw
+	const objects = [ourObject];
 
-// Tell EasyRender about the objects you would like to render
-ERInitScene(objects);
+	// Tell EasyRender about the objects you would like to render
+	ERInitScene(objects);
 
-// Start the render loop
-ERBeginRenderLoop();
-console.log(ourObject);
+	// Start the render loop
+	ERBeginRenderLoop();
+}
+
+main();
