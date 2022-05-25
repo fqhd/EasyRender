63a6fc9656001da4a6c45e588009fe7a15007053
fqhd <fahd31256@gmail.com>
2022-05-25 10:20:49 +0100

    add model loading/rendering

--- easy_render.js
+++ easy_render.js
@@ -83,7 +83,7 @@ function createRawModel(positions, normals, indices) {
 		gl.STATIC_DRAW
 	);
 
-	numPositions = indices.length;
+	numPositions = indices.length / 3;
 
 	return {
 		buffers: {
@@ -126,7 +126,7 @@ function createTexturedModel(positions, normals, indices, textureCoords) {
 		gl.STATIC_DRAW
 	);
 
-	numPositions = indices.length;
+	numPositions = indices.length / 3;
 
 	return {
 		buffers: {
@@ -217,17 +217,148 @@ async function ERLoadTexture(url) {
 	});
 }
 
+async function ERLoadModel(url) {
+	const data = await loadModelData(url);
+	optimizeModel(data);
+	return data;
+}
+
+function optimizeModel(data) {
+	const indices = [];
+	for (let i = 0; i < data.positions.length; i++) {
+		indices.push(i);
+	}
+	data.indices = indices;
+}
+
+async function loadModelData(url) {
+	const response = await fetch(url);
+	const text = await response.text();
+	const lines = text.split("\n");
+	const positions_lookup = [];
+	const normals_lookup = [];
+	const uvs_lookup = [];
+	for (let i = 0; i < lines.length; i++) {
+		let tokens = lines[i].split(" ");
+		switch (tokens[0]) {
+			case "v":
+				positions_lookup.push(parseFloat(tokens[1]));
+				positions_lookup.push(parseFloat(tokens[2]));
+				positions_lookup.push(parseFloat(tokens[3]));
+				break;
+			case "vn":
+				normals_lookup.push(parseFloat(tokens[1]));
+				normals_lookup.push(parseFloat(tokens[2]));
+				normals_lookup.push(parseFloat(tokens[3]));
+				break;
+			case "vt":
+				uvs_lookup.push(parseFloat(tokens[1]));
+				uvs_lookup.push(parseFloat(tokens[2]));
+				break;
+		}
+	}
+
+	const positions = [];
+	const normals = [];
+	const textureCoords = [];
+
+	for (let i = 0; i < lines.length; i++) {
+		let tokens = lines[i].split(" ");
+		if (tokens[0] == "f") {
+			for (let j = 1; j < 4; j++) {
+				const curr_token_indices = tokens[j].split("/").map((t) => parseInt(t));
+
+				const pos_index = curr_token_indices[0] - 1;
+				positions.push(positions_lookup[pos_index * 3]);
+				positions.push(positions_lookup[pos_index * 3 + 1]);
+				positions.push(positions_lookup[pos_index * 3 + 2]);
+
+				const uv_index = curr_token_indices[1] - 1;
+				textureCoords.push(uvs_lookup[uv_index * 2]);
+				textureCoords.push(uvs_lookup[uv_index * 2 + 1]);
+
+				const normal_index = curr_token_indices[2] - 1;
+				normals.push(normals_lookup[normal_index * 3]);
+				normals.push(normals_lookup[normal_index * 3 + 1]);
+				normals.push(normals_lookup[normal_index * 3 + 2]);
+			}
+		}
+	}
+
+	const tangents = calc_tangents(positions, textureCoords);
+
+	return {
+		positions,
+		normals,
+		textureCoords,
+		tangents,
+	};
+}
+
+function sub_vec2(a, b) {
+	return [a[0] - b[0], a[1] - b[1]];
+}
+
+function sub_vec3(a, b) {
+	return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+
+function mul_vec3(a, b) {
+	return [a[0] * b, a[1] * b, a[2] * b];
+}
+
+function calc_tangents(positions, uvs) {
+	const tangents = [];
+
+	let j = 0;
+
+	for (let i = 0; i < positions.length; i += 9) {
+		const P1 = [positions[i], positions[i + 1], positions[i + 2]];
+		const P0 = [positions[i + 3], positions[i + 4], positions[i + 5]];
+		const P2 = [positions[i + 6], positions[i + 7], positions[i + 8]];
+
+		const T1 = [uvs[j], uvs[j + 1]];
+		const T0 = [uvs[j + 2], uvs[j + 3]];
+		const T2 = [uvs[j + 4], uvs[j + 5]];
+		j += 6;
+
+		const delta_pos_1 = sub_vec3(P1, P0);
+		const delta_pos_2 = sub_vec3(P2, P0);
+
+		const delta_uv_1 = sub_vec2(T1, T0);
+		const delta_uv_2 = sub_vec2(T2, T0);
+
+		const r =
+			1 / (delta_uv_1[0] * delta_uv_2[1] - delta_uv_1[1] * delta_uv_2[0]);
+
+		const tangent = mul_vec3(
+			sub_vec3(
+				mul_vec3(delta_pos_1, delta_uv_2[1]),
+				mul_vec3(delta_pos_2, delta_uv_1[1])
+			),
+			r
+		);
+
+		tangents.push(tangent[0]);
+		tangents.push(tangent[1]);
+		tangents.push(tangent[2]);
+		tangents.push(tangent[0]);
+		tangents.push(tangent[1]);
+		tangents.push(tangent[2]);
+		tangents.push(tangent[0]);
+		tangents.push(tangent[1]);
+		tangents.push(tangent[2]);
+	}
+
+	return tangents;
+}
+
 function isPowerOf2(value) {
 	return (value & (value - 1)) == 0;
 }
 
-function ERBeginRenderLoop() {
-	drawScene();
-	requestAnimationFrame(ERBeginRenderLoop);
-}
-
-function drawScene() {
-	gl.clear(gl.COLOR_BUFFER_BIT);
+function ERDrawScene() {
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	gl.useProgram(modelShader.program);
 	updateCamera();
 	for (const object of ERObjects) {
@@ -383,6 +514,7 @@ function initWebGL() {
 
 function initGLState() {
 	gl.clearColor(0, 0, 0, 1);
+	gl.enable(gl.DEPTH_TEST);
 }
 
 function createShaderProgram(vSource, fSource) {
--- main.js
+++ main.js
@@ -2,19 +2,13 @@ async function main() {
 	// Initialize the library
 	ERInit();
 
-	// Define the positions, normals, and textureCoords of your model (These will generally be loaded from a file)
-	const positions = [0, 0, 0, 0.5, 1, 0, 1, 0, 0];
-	const normals = [0, 0, -1, 0, 0, -1, 0, 0, -1];
-	const textureCoords = [0, 0, 0.5, 1, 1, 0];
-	const indices = [0, 1, 2];
-
-	// Define the textureData of your model(This will also generally be loaded in from a file)
-	const textureData = [
-		0,
-		0,
-		255,
-		255, // This will make 1 opaque blue pixel
-	];
+	const { positions, normals, indices, textureCoords } = await ERLoadModel(
+		"./model.obj"
+	);
+	console.log(positions);
+	console.log(normals);
+	console.log(indices);
+	console.log(textureCoords);
 
 	const model = ERCreateModel(positions, normals, indices, textureCoords);
 	const texture = await ERLoadTexture("./bricks.png");
@@ -27,7 +21,12 @@ async function main() {
 	ERInitScene(objects);
 
 	// Start the render loop
-	ERBeginRenderLoop();
+	beginRenderLoop();
+}
+
+function beginRenderLoop() {
+	ERDrawScene();
+	requestAnimationFrame(beginRenderLoop);
 }
 
 main();
