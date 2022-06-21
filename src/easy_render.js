e4b59353dda70513c45532ff8f28afc54f0103ac
fqhd <fahd31256@gmail.com>
2022-06-21 19:30:51 +0100

    fix: remove useless function

--- src/easy_render.js
+++ src/easy_render.js
@@ -850,15 +850,6 @@ function updateModelMatrix(transform) {
 	mat4.scale(transform.matrix, transform.matrix, transform.scale);
 }
 
-const getImageData = (image) => {
-	const canvas = document.createElement("canvas");
-	const context = canvas.getContext("2d");
-	canvas.width = image.width;
-	canvas.height = image.height;
-	context.drawImage(image, 0, 0);
-	return context.getImageData(0, 0, canvas.width, canvas.height);
-};
-
 function createShadowMapShader() {
 	const vSource = `
 	attribute vec3 aPosition;
